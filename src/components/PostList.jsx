e58d791c5bfccb829885542e0c6b06878307b0ec
Dmitriy Skorik <dmitriy.skorik@devit-team.com>
2022-01-19 22:43:57 +0200

    include react-transition-group to post list

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PostItem from "./PostItem";
+import {TransitionGroup, CSSTransition} from "react-transition-group";
 
 const PostList = ({posts, title, remove}) => {
 
@@ -16,10 +17,17 @@ const PostList = ({posts, title, remove}) => {
             <h1 style={{textAlign: 'center'}}>
                 {title}
             </h1>
-
-            {posts.map((post, index) =>
-                <PostItem number={index + 1} post={post} key={post.id} remove={remove}/>
-            )}
+            <TransitionGroup>
+                {posts.map((post, index) =>
+                    <CSSTransition
+                        key={post.id}
+                        timeout={500}
+                        classNames="post"
+                    >
+                        <PostItem number={index + 1} post={post} remove={remove}/>
+                    </CSSTransition>
+                )}
+            </TransitionGroup>
         </div>
     );
 };
